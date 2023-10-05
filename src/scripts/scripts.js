56de6e89700bed08c293b5a330e47e6ea999df96
Anton Shchukin <a.a.shchukin@gmail.com>
2023-10-05 16:43:11 +0700

    header

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,16 @@
 (function($) {
 
+    const $html = $('html');
 
+    const isScrolled = () => {
+        if ($(window).scrollTop() > 80) {
+            $html.addClass('scrolled')
+        } else  {
+            $html.removeClass('scrolled')
+        }
+    }
+
+    $(window).on('scroll', isScrolled);
+    $(document).ready(isScrolled);
 
 })(jQuery);
