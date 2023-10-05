9b3f3a18cb6d235c5d74ae5d9071b049c666a574
Anton Shchukin <a.a.shchukin@gmail.com>
2023-10-05 17:31:18 +0700

    smooth scrolling

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,16 +1,30 @@
-(function($) {
+(function ($) {
+
+    /* Header color scheme */
 
     const $html = $('html');
 
     const isScrolled = () => {
         if ($(window).scrollTop() > 80) {
             $html.addClass('scrolled')
-        } else  {
+        } else {
             $html.removeClass('scrolled')
         }
     }
 
     $(window).on('scroll', isScrolled);
     $(document).ready(isScrolled);
+
+
+    /* Smooth scrolling */
+
+    const $header = $('.header');
+
+    $('.header__link').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $(this).attr("href") ).offset().top - $header.outerHeight()
+        }, 600);
+    });
 
 })(jQuery);
