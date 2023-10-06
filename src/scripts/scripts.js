2d1d6d72761f2fa2a2e1a8e95659a8645fd4ed75
Anton Shchukin <a.a.shchukin@gmail.com>
2023-10-06 16:58:31 +0700

    slick draft

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,5 +1,40 @@
 (function ($) {
 
+    /* Slick init */
+
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        gap: 10,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
     /* Header color scheme */
 
     const $html = $('html');
